faccf26ae87aa7a54566d9e1a1d2544040347fd0
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-06-28 19:35:29 +0200

    Editing Form.

--- Vidly/ViewModels/NewCustomerViewModel.cs
+++ Vidly/ViewModels/NewCustomerViewModel.cs
@@ -6,9 +6,11 @@ using Vidly.Models;
 
 namespace Vidly.ViewModels
 {
-    public class NewCustomerViewModel
+    public class CustomerFormViewModel
     {
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
         public Customer Customer { get; set; }
     }
 }
+
+
