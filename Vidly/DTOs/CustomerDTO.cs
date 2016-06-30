7b53b0ced0ddcd37775661b159fac10c8de4b913
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-06-30 18:27:30 +0200

    Added DTO class for customer.

---
+++ Vidly/DTOs/CustomerDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Vidly.Models;
+
+namespace Vidly.DTOs
+{
+    public class CustomerDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        public byte MembershipTypeId { get; set; }
+
+        [Min18YearsIfMember]
+        public DateTime? Birthday { get; set; }
+    }
+}
