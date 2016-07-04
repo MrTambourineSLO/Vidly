29a835fbdf347378ccfdaec1a13c327ba819b975
Saso Miocinovic <saso.miocinovic@gmail.com>
2016-07-04 20:15:38 +0200

    Added plumbing for autocomplete.

--- Vidly/App_Start/BundleConfig.cs
+++ Vidly/App_Start/BundleConfig.cs
@@ -14,7 +14,8 @@ namespace Vidly
                         "~/scripts/bootbox.js",
                         "~/Scripts/respond.js",
                         "~/Scripts/datatables/jquery.dataTables.js",
-                        "~/Scripts/datatables/dataTables.bootstrap.js"));
+                        "~/Scripts/datatables/dataTables.bootstrap.js",
+                        "~/Scripts/typeahead.bundle.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -27,7 +28,8 @@ namespace Vidly
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css",
-                      "~/Content/datatables/css/datatables.bootstrap.css"));
+                      "~/Content/datatables/css/datatables.bootstrap.css",
+                      "~/Content/typeahead.css"));
         }
     }
 }
